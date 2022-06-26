04e850ac57f31c0bd329be840e033cfae05811ac
johannkrugell <johannkrugell@gmail.com>
2022-06-26 20:52:40 +1000

    ADD CSS STYLING, BUTTON AND CHECK NO OF SQUARES Add CSS styling to the page and a button to clear the canvas and request from the user how many squares should be drawn on the page.
    On branch button-styling
    Changes to be committed:
    modified:   css/stylesheet.css
    new file:   img/unicorn-gfea0fb8df_1280.png
    new file:   img/unicorn-gfea0fb8df_1280.png:Zone.Identifier
    modified:   index.html
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-createGrid();
+/* Button that allows the user to set how many squares are on the canvas 
+also resets the canvas if clicked again
+*/
+let createSquareButton = document.getElementById("buttoncreate")
+createSquareButton.addEventListener("click",function(){  
+  if (document.getElementById("square") == null) {
+    createGrid();
+  } else {
+    document.querySelectorAll(".square").forEach(function (element){
+      element.remove();
+    })
+  }
+})
 
 /* the canvas or paper size is set to 600px the function calculates the
 size of each square by dividing the canvas size by the grid size the user
@@ -6,22 +18,28 @@ entered
 */
 function createGrid() {
   let gridSize = prompt("How many squares per side?");
+  if (gridSize > 100) {
+    gridSize = prompt("Please enter a number less than or equal to a 100")
+  } 
   for (let i = 0; i < gridSize*gridSize; i++) {
     const square = document.createElement("div");
     const paper = document.getElementById("container");
     square.classList.add("square");
+    square.setAttribute("id","square");
     square.style.width = `${((600-((gridSize*2)))/ gridSize)}px`;
     square.style.height = `${((600-((gridSize*2)))/ gridSize)}px`;
     paper.appendChild(square); 
+    
     // add event lister that call function to random color on hover
     square.addEventListener("mouseover", function(){
-      square.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})` 
+      square.style.backgroundColor = `rgb(${randomColor()},${randomColor()},
+                                          ${randomColor()})` 
     }  
       ,false)}  
 }
 
-// function that generates a random number between 1 and 255 that is used to 
-// generate the color of the square
+/* Function that generates a random number between 1 and 255 that is used to 
+generate the color of the square */
 function randomColor(){
   return Math.floor((Math.random()*255) +1)
 }
